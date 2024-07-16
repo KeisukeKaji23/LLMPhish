3e7e1609b63a988853c2417e92f1bf2f174050de
KeisukeKaji23 <234t216t@gsuite.kobe-u.ac.jp>
2024-07-16 16:38:16 +0900

    [fix] main関数を修正 絶対パスを対応

--- main.py
+++ main.py
@@ -12,39 +12,42 @@ retreat_visual_data(
 )
 
 
-# def main(role, website_type, upper_limit):
-#     if website_type == "phishing" or website_type == "benign":
-#         with open("contet.json", "r") as f:
-#             json_conetnt = json.load(f)
-#             content = json_conetnt[role]
-#             with open(
-#                 f"/home/keisukekaji/my-study/dataset/{website_type}/site_visual_dict.json",
-#                 "r",
-#             ) as f:
-#                 site_visual_dict = json.load(f)
-#                 model_name = "gpt-4-turbo"
-#                 judge_website(
-#                     content,
-#                     site_visual_dict,
-#                     model_name,
-#                     f"/home/keisukekaji/my-study/result/{website_type}/{role}_{website_type}_result.json",
-#                     upper_limit,
-#                 )
-#     else:
-#         with open("other_content.json", "r") as f:
-#             json_conetnt = json.load(f)
-#             content = json_conetnt[role]
-#             with open(
-#                 f"/home/keisukekaji/my-study/dataset/{website_type}/site_visual_dict.json",
-#                 "r",
-#             ) as f:
-#                 site_visual_dict = json.load(f)
-#                 model_name = "gpt-4-turbo"
-#                 judge_website(
-#                     content,
-#                     website_type,
-#                     site_visual_dict,
-#                     model_name,
-#                     f"/home/keisukekaji/my-study/result/{website_type}/{role}_{website_type}_result.json",
-#                     upper_limit,
-#                 )
+def main(role, website_type, upper_limit):
+    if website_type == "phishing" or website_type == "benign":
+        with open("contet.json", "r") as f:
+            json_conetnt = json.load(f)
+            content = json_conetnt[role]
+            with open(
+                f"/home/keisukekaji/my-study/dataset/{website_type}/site_visual_dict.json",
+                "r",
+            ) as f:
+                site_visual_dict = json.load(f)
+                model_name = "gpt-4-turbo"
+                judge_website(
+                    content,
+                    site_visual_dict,
+                    model_name,
+                    f"/home/keisukekaji/my-study/result/{website_type}/{role}_{website_type}_result.json",
+                    upper_limit,
+                )
+    else:
+        with open("other_content.json", "r") as f:
+            json_conetnt = json.load(f)
+            content = json_conetnt[role]
+            with open(
+                f"{root_dir}/site_visual_dict.json",
+                "r",
+            ) as f:
+                site_visual_dict = json.load(f)
+                model_name = "gpt-4-turbo"
+                judge_website(
+                    content,
+                    website_type,
+                    site_visual_dict,
+                    model_name,
+                    f"{root_dir}/{role}_{website_type}_result.json",
+                    upper_limit,
+                )
+
+
+main("expert", "other", 100)
